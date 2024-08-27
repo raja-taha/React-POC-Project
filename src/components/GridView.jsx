f93a0316969deb32d677919c00af2b54690fbc42
raja-taha <rajataha062@gmail.com>
2024-08-27 10:01:15 +0500

    Mobile Friendly

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const GridView = ({ coins }) => {
+const GridView = ({ coins, onSelect }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-10 gap-4 p-4">
       {coins.map((coin) => (
         <div
           key={coin.id}
+          onClick={() => onSelect(coin)}
           className="flex flex-col items-center justify-center gap-2 p-4 border border-grey rounded-lg shadow-sm hover:shadow-lg transition-shadow"
         >
           <img src={coin.image} alt="coin" width={50} />
